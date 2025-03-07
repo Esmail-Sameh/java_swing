5bd2ac64d099287640da77e474c9e9d9927d9f1d
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-03-07 02:27:42 +0200

    first commit

---
+++ src/main/java/com/mycompany/java_swing/Java_swing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_swing;
+
+/**
+ *
+ * @author 11
+ */
+public class Java_swing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
